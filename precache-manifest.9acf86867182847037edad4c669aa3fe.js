31b74fdc867577bc6b3de1960172576b9c57472a
Yash <thakuryash@live.com>
2019-06-18 16:13:30 -0400

    Updates

---
+++ precache-manifest.9acf86867182847037edad4c669aa3fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f03879692d2623ed2d9d137db6b90c7",
+    "url": "/OpenTable-ReactApp/index.html"
+  },
+  {
+    "revision": "552a751de5d3da7f7780",
+    "url": "/OpenTable-ReactApp/static/css/main.ed69065f.chunk.css"
+  },
+  {
+    "revision": "de707db0a611ced70c26",
+    "url": "/OpenTable-ReactApp/static/js/2.eb636cfe.chunk.js"
+  },
+  {
+    "revision": "552a751de5d3da7f7780",
+    "url": "/OpenTable-ReactApp/static/js/main.1ee6a995.chunk.js"
+  },
+  {
+    "revision": "b3d4202dde5517854caa",
+    "url": "/OpenTable-ReactApp/static/js/runtime~main.3a6efadc.js"
+  }
+]);
